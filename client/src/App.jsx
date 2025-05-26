46e556f0a0f175201e9efe5d3d4b31966871e73c
shobuj <shobuj@gmail.com>
2025-05-26 20:18:07 +0600

    creating home page with hero component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import {useLocation} from 'react-router-dom'
+import {Routes,Route, useLocation} from 'react-router-dom'
+import Home from './pages/Home'
 
 function App() {
    // If we navigate to the owner, the navbar will not appear. 
@@ -8,6 +9,16 @@ function App() {
   return (
     <div className=''>
      { !isOwnerPath && <Navbar /> }
+     <div className='min-h-[70vh]'>
+       <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/rooms' element={<h1>Rooms Page</h1>} />
+          <Route path='/experience' element={<h1>Experience Page</h1>} />
+          <Route path='/about' element={<h1>About Page</h1>} />
+          <Route path='/owner' element={<h1>Owner Dashboard</h1>} />
+          {/* Add more routes as needed */}
+       </Routes>
+     </div>
     </div>
   )
 }
