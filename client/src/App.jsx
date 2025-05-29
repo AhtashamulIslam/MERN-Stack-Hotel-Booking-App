bf16d7e3ef2e45fe4b146053044c06a7d76201d7
shobuj <shobuj@gmail.com>
2025-05-29 21:44:16 +0600

    complete the room detail page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,17 +4,19 @@ import {Routes,Route, useLocation} from 'react-router-dom'
 import Home from './pages/Home'
 import Footer from './components/Footer'
 import AllRooms from './pages/AllRooms'
+import RoomDetails from './pages/RoomDetails'
 
 function App() {
    // If we navigate to the owner, the navbar will not appear. 
    const isOwnerPath = useLocation().pathname.includes('owner')
   return (
     <div className=''>
-     { !isOwnerPath && <Navbar /> }
+     { !isOwnerPath && <Navbar /> } 
      <div className='min-h-[70vh]'>
        <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/rooms' element={<AllRooms />} />
+          <Route path='/rooms/:id' element={<RoomDetails />} />
           <Route path='/owner' element={<h1>Owner Dashboard</h1>} />
           {/* Add more routes as needed */}
        </Routes>
